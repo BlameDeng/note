3afe1986b3d3ab2bf109f9a090bdaf901699dea5
BlameDeng <476961779@qq.com>
2018-09-25 12:03:01 +0800

    pop定位bug

--- src/components/sider/Sider.vue
+++ src/components/sider/Sider.vue
@@ -9,35 +9,35 @@
                     <p ref="notebook" @click="onAddNotebook">新建文件夹</p>
                 </div>
             </div>
-            <div class="recent" :class="{active:selectedTab==='recent'}" @click="onClickTab($event,'recent')">
+            <div class="recent" :class="{active:selectedTab==='recent'&&!selectedBook}" @click="onClickTab($event,'recent')">
                 <n-icon name="new" class="icon"></n-icon>
                 <span>最新文档</span>
             </div>
-            <div class="books" :class="{active:selectedTab==='books'}" @click="onClickTab($event,'books')">
+            <div class="books" :class="{active:selectedTab==='books'&&!selectedBook}" @click="onClickTab($event,'books')">
                 <p @click="toggleCollect" :class="{active:!retractNotebooks}" ref="wenjianjiaP"></p>
                 <n-icon name="wenjianjia" class="icon"></n-icon>
                 <span>我的文件夹</span>
             </div>
-            <div class="book" v-for="(book,index) in allNotebooks" :key="book.id" v-if="allNotebooks&&allNotebooks.length&&!retractNotebooks" @click.right="onRightNotebook($event,index,book)" :class="{active:index===clickNotebookIndex}" @click="onLeftNotebook(index)">
-                <template v-if="book.id!==renameNotebookId">
+            <div class="book" v-for="(book,index) in allNotebooks" :key="book.id" v-if="allNotebooks&&allNotebooks.length&&!retractNotebooks" :class="{active:book===selectedBook}" @click="onClickBook($event,index,book)" @click.right="onClickBook($event,index,book)">
+                <template v-if="book!==renameBook">
                     <n-icon name="wenjian" class="icon"></n-icon>
                     <span>{{book.title}}</span>
-                    <p @click="onRightNotebook($event,index,book)" ref="wenjiaP"></p>
-                    <div class="popover" ref="bookPop" :class="{active:book.id===selectedNotebookId}">
-                        <p ref="rename" @click="onRenameNotebook(book)">重命名</p>
+                    <p @click.stop="onClickBook($event,index,book)" ref="wenjiaP"></p>
+                    <div class="book-pop" ref="bookPop" v-show="showBookPop&&book===selectedBook">
+                        <p ref="rename" @click="onClickRenameBook(book)">重命名</p>
                         <p ref="cancle" @click="onDeleteNotebooks(book)">删除</p>
                     </div>
                 </template>
                 <template v-else>
                     <n-icon name="wenjian" class="icon"></n-icon>
-                    <input type="text" v-model="notebooksNewName" autofocus="autofocus" @focus="onFocus" @blur="onSubmitRenameNotebook(book)">
+                    <input type="text" v-model="newBookTitle" autofocus="autofocus" @focus="onFocus" @blur="onRenameBook(book)">
                 </template>
             </div>
             <div class="book" v-if="showNewNotebook">
                 <n-icon name="wenjian" class="icon"></n-icon>
-                <input type="text" v-model="newNotebookName" autofocus="autofocus" @focus="onFocus" @blur="onSubmitAddNotebook">
+                <input type="text" v-model="newName" autofocus="autofocus" @focus="onFocus" @blur="onSubmitAddNotebook">
             </div>
-            <div class="trash" :class="{active:selectedTab==='trash'}" @click="onClickTab($event,'trash')">
+            <div class="trash" :class="{active:selectedTab==='trash'&&!selectedBook}" @click="onClickTab($event,'trash')">
                 <n-icon name="trash" class="icon"></n-icon>
                 <span>回收站</span>
                 <p></p>
@@ -62,131 +62,151 @@
     </div>
 </template>
 <script>
-    import Icon from '../icon.vue'
-    import { mapState, mapActions, mapMutations } from 'vuex'
-    export default {
-        name: 'Sider',
-        components: { 'n-icon': Icon },
-        data() {
-            return {
-                showAddPop: false,
-                showNewNotebook: false,
-                newNotebookName: '新文件夹',
-                selectedTab: '',
-                selectedNotebookId: '',
-                clickNotebookIndex: -1,
-                rightNotebookIndex: -1,
-                notebooksNewName: '',
-                renameNotebookId: '',
-                retractNotebooks: true
-            }
-        },
-        inject:['eventBus'],
-        computed: {
-            ...mapState({
-                isLogin: state => state.auth.isLogin,
-                allNotebooks: state => state.notebooks.allNotebooks
-            })
-        },
-        methods: {
-            ...mapActions(['logout', 'getNotebooks', 'createNotebooks',
-                'deleteNotebooks', 'renameNotebooks'
-            ]),
-            ...mapMutations(['addNotebooks', 'filterNotebooks', 'updateNotebooks']),
-            onAddNote() { this.eventBus.$emit('add-note'); },
-            onClickAdd() { this.showAddPop = true; },
-            onClickTab(e, tab) {
-                this.selectedTab = tab;
-                this.selectedNotebookId = '';
-                this.clickNotebookIndex = -1;
-                this.rightNotebookIndex = -1;
-                if (tab === 'books' && e.target !== this.$refs.wenjianjiaP) {
-                    this.retractNotebooks = false;
-                }
-            },
-            toggleCollect() { this.retractNotebooks = !this.retractNotebooks; },
-            listenAddPop(e) {
-                let note = this.$refs.note;
-                let notebook = this.$refs.notebook;
-                if (e.target === note || e.target === notebook) { return } else { this.showAddPop = false; }
-            },
-            listenBookPop(e) {
-                let rename = this.$refs.rename[this.rightNotebookIndex];
-                let cancle = this.$refs.cancle[this.rightNotebookIndex];
-                if (e.target === rename || e.target === cancle) { return } else { this.selectedNotebookId = ''; }
-            },
-            onFocus(e) { e.target.select(); },
-            onAddNotebook() { this.showNewNotebook = true; },
-            onSubmitAddNotebook() {
-                this.createNotebooks({ title: this.newNotebookName }).then(res => {
-                    this.showNewNotebook = false;
-                    this.newNotebookName = '新文件夹';
-                    this.addNotebooks({ notebook: res.data });
-                    this.retractNotebooks=false;
-                })
-            },
-            onDeleteNotebooks(notebook) {
-                this.deleteNotebooks({ notebookId: notebook.id }).then(res => {
-                    this.filterNotebooks({ id: notebook.id });
-                })
-            },
-            onRightNotebook(e, index, notebook) {
-                let { clientX: x, clientY: y } = e;
-                this.notebookPop(x, y, index);
-                this.selectedNotebookId = notebook.id;
-                this.clickNotebookIndex = index;
-                this.rightNotebookIndex = index;
-            },
-            notebookPop(x, y, index) {
-                let pop = this.$refs.bookPop[index];
-                pop.style.top = y + 'px';
-                pop.style.left = x + 'px';
-            },
-            onLeftNotebook(index) {
-                this.clickNotebookIndex = index;
-            },
-            onRenameNotebook(notebook) {
-                this.notebooksNewName = notebook.title;
-                this.renameNotebookId = notebook.id;
-            },
-            onSubmitRenameNotebook(notebook) {
-                this.renameNotebooks({ title: this.notebooksNewName, notebookId: this.renameNotebookId })
-                    .then(res => {
-                        notebook.title = this.notebooksNewName;
-                        this.updateNotebooks({ notebook });
-                        this.notebooksNewName = '';
-                        this.renameNotebookId = '';
-                    })
-            }
-        },
-        watch: {
-            showAddPop(val) {
-                if (val) {
-                    document.addEventListener('click', this.listenAddPop);
-                } else {
-                    document.removeEventListener('click', this.listenAddPop);
-                }
-            },
-            selectedNotebookId(val) {
-                if (val) {
-                    document.addEventListener('click', this.listenBookPop);
-                } else {
-                    document.removeEventListener('click', this.listenBookPop);
-                }
-            },
-            clickNotebookIndex(val) {
-                if (val > -1) {
-                    this.selectedTab = '';
-                }
-            }
-        },
-        mounted() {
-            this.$el.oncontextmenu = () => { return false; };
-        },
-        beforeDestroy() {
-            document.removeEventListener('click', this.listenAddPop);
-            document.removeEventListener('click', this.listenBookPop);
-        }
+import Icon from "../icon.vue";
+import { mapState, mapActions, mapMutations } from "vuex";
+export default {
+  name: "Sider",
+  components: { "n-icon": Icon },
+  data() {
+    return {
+      showAddPop: false,
+      showBookPop: false,
+      showNewNotebook: false,
+      newName: "新文件夹",
+      selectedTab: "",
+      selectedBook: null,
+      renameBook: null,
+      newBookTitle: "",
+      retractNotebooks: true
+    };
+  },
+  inject: ["eventBus"],
+  computed: {
+    ...mapState({
+      isLogin: state => state.auth.isLogin,
+      allNotebooks: state => state.notebooks.allNotebooks
+    })
+  },
+  methods: {
+    ...mapActions([
+      "logout",
+      "getNotebooks",
+      "createNotebooks",
+      "deleteNotebooks",
+      "renameNotebooks"
+    ]),
+    ...mapMutations(["addNotebooks", "filterNotebooks", "updateNotebooks"]),
+    onAddNote() {
+      this.eventBus.$emit("add-note");
+    },
+    onClickAdd() {
+      this.showAddPop = true;
+    },
+    onClickTab(e, tab) {
+      this.selectedTab = tab;
+      this.selectedBook = null;
+      if (tab === "books" && e.target !== this.$refs.wenjianjiaP) {
+        this.retractNotebooks = false;
+      }
+    },
+    toggleCollect() {
+      this.retractNotebooks = !this.retractNotebooks;
+    },
+    listenAddPop(e) {
+      let note = this.$refs.note;
+      let notebook = this.$refs.notebook;
+      if (e.target === note || e.target === notebook) {
+        return;
+      } else {
+        this.showAddPop = false;
+      }
+    },
+    listenBookPop(e) {
+      let rename = this.$refs.rename;
+      let cancle = this.$refs.cancle;
+      if (e.target === rename || e.target === cancle) {
+        return;
+      } else {
+        this.showBookPop = false;
+      }
+    },
+    onFocus(e) {
+      e.target.select();
+    },
+    onAddNotebook() {
+      this.showNewNotebook = true;
+    },
+    onSubmitAddNotebook() {
+      this.createNotebooks({ title: this.newName }).then(res => {
+        this.showNewNotebook = false;
+        this.newName = "新文件夹";
+        this.addNotebooks({ notebook: res.data });
+        this.retractNotebooks = false;
+      });
+    },
+    onDeleteNotebooks(notebook) {
+      this.deleteNotebooks({ notebookId: notebook.id }).then(res => {
+        this.filterNotebooks({ id: notebook.id });
+      });
+    },
+    onClickBook(e, index, book) {
+      let { clientX: x, clientY: y, which } = e;
+      console.log(x,y,index)
+      this.selectedBook = book;
+      if (which === 1 && e.target.tagName !== "P") {
+        return;
+      } else {
+        this.notebookPop(x, y, index);
+      }
+    },
+    notebookPop(x, y, index) {
+      let pop = this.$refs.bookPop[index];
+      console.log(pop)
+      pop.style.top = y + "px";
+      pop.style.left = x + "px";
+      this.showBookPop = true;
+    },
+    onClickRenameBook(book) {
+      this.renameBook = book;
+      this.selectedBook = null;
+      this.newBookTitle = book.title;
+    },
+    onRenameBook(book) {
+      this.renameNotebooks({
+        title: this.newBookTitle,
+        notebookId: book.id
+      }).then(res => {
+        book.title = this.newBookTitle;
+        this.updateNotebooks({ notebook: book });
+        this.renameBook = null;
+      });
     }
+  },
+  watch: {
+    showAddPop(val) {
+      if (val) {
+        document.addEventListener("click", this.listenAddPop);
+      } else {
+        document.removeEventListener("click", this.listenAddPop);
+      }
+    },
+    showBookPop(val) {
+      if (val) {
+        document.addEventListener("click", this.listenBookPop);
+      } else {
+        document.removeEventListener("click", this.listenBookPop);
+      }
+    }
+  },
+  mounted() {
+    this.$el.oncontextmenu = () => {
+      return false;
+    };
+  },
+  beforeDestroy() {
+    document.removeEventListener("click", this.listenAddPop);
+    document.removeEventListener("click", this.listenBookPop);
+  }
+};
 </script>
 <style lang="scss" scoped src='./Sider.scss'></style>
